46392129e45725baa5245c77d7de1510bb6aa788
ttyS3 <ttys3.rust@gmail.com>
2023-06-13 11:33:47 +0800

    chore: colored message output

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,16 @@ impl RedirectionDetector {
     }
 }
 
+fn print_colored_message(message: &str, color_code: u8) {
+    // ANSI escape codes for coloring the output
+    const COLOR_START: &str = "\x1B[38;5;";
+    const COLOR_END: &str = "m";
+    const RESET_COLOR: &str = "\x1B[0m";
+
+    let colored_message = format!("{}{}{}", COLOR_START, color_code, COLOR_END);
+    println!("{}{}{}", colored_message, message, RESET_COLOR);
+}
+
 fn main() {
     let args = Cli::parse();
 
@@ -54,9 +64,10 @@ fn main() {
 
     let detector = RedirectionDetector::new(timeout_secs);
 
+    // https://www.ditig.com/256-colors-cheat-sheet
     match detector.detect(&args.url) {
-        Ok(Some(uri)) => println!("Redirect URI: {}", uri),
-        Ok(None) => println!("No redirect occurred."),
-        Err(err) => println!("Error: {}", err),
+        Ok(Some(uri)) => print_colored_message(&format!("Redirect URI: {}", uri), 41), // SpringGreen3	#00d75f
+        Ok(None) => print_colored_message("No redirect occurred.", 39), // DeepSkyBlue1	#00afff	
+        Err(err) => print_colored_message(&format!("Error: {}", err), 202), //OrangeRed1	#ff5f00
     }
 }
